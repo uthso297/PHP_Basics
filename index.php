48a1f12a15f6274bebd3fb5e6b0d6a08407c1623
uthso297 <uthso119464@gmail.com>
2026-04-07 01:58:48 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Hello PHP <br>";
+    echo 'Hello World <br>';
+    echo 'Hello '.'World <br>';
+    ?>
+</body>
+</html>
