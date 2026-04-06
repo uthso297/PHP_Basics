b1a8ce900ab4eccf0b31e501b035ec974004fb6d
uthso297 <uthso119464@gmail.com>
2026-04-07 03:04:22 +0600

    loops

---
+++ loops.php
@@ -0,0 +1,20 @@
+<?php
+
+// For loop
+// for ($i = 1; $i <= 5; $i++) {
+//     echo $i . " ";
+// }
+
+// while loop
+// $i = 1;
+// while ($i <= 5) {
+//     echo $i . " ";
+//     $i++;
+// }
+
+// foreach loop
+$fruits = ["Apple", "Banana", "Cherry"];
+
+foreach ($fruits as $fruit) {
+    echo $fruit . " ";
+}
